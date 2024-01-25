b703c35dfe92a8550dc19a28437c66a552985433
camila modena <itscamilla@hotmail.com>
2024-01-25 14:36:25 -0300

    school carousel

--- app/javascript/controllers/book_controller.js
+++ app/javascript/controllers/book_controller.js
@@ -2,14 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="book"
 export default class extends Controller {
-  static targets = ["cover", "page", "leftArrow", "rightArrow"]
+  static targets = ["cover", "page", "leftArrow", "rightArrow", "leftArrowSchool", "rightArrowSchool", "schoolImage"]
 
   connect() {
     this.pageNumber = 0
-    // this.leftArrowTarget.style.opacity = 0
+    this.imageNumber = 0
   }
 
-  forward() {
+  forwardBook() {
     if (this.pageNumber == 19) {
       this.rightArrowTarget.style.opacity = 0
     }
@@ -34,7 +34,7 @@ export default class extends Controller {
     }
   }
 
-  backward() {
+  backwardBook() {
     if (this.pageNumber >= 2) {
       this.pageTargets.forEach((page) => {
         if (page.dataset.status == "active") {
@@ -58,6 +58,60 @@ export default class extends Controller {
 
     if (this.pageNumber == 19) {
       this.rightArrowTarget.style.opacity = 1
+    }
+  }
+
+  forwardSchool() {
+    if (this.imageNumber < 4) {
+      this.schoolImageTargets.forEach((image) => {
+        if (image.dataset.status == "active") {
+          image.dataset.status = "inactive"
+          image.classList.add("hidden")
+          this.imageNumber = parseInt(image.dataset.image, 10) + 1
+        }
+
+        if (image.dataset.image == this.imageNumber) {
+          image.classList.remove("hidden")
+          image.dataset.status = "active"
+        }
+      })
+    }
+
+    if (this.imageNumber > 1) {
+      this.leftArrowSchoolTarget.style.opacity = 1
+    }
+
+    if (this.imageNumber == 4) {
+      this.rightArrowSchoolTarget.style.opacity = 0
+    }
+
+    console.log(this.imageNumber)
+  }
+
+  backwardSchool() {
+    if (this.imageNumber >= 2) {
+      this.schoolImageTargets.forEach((image) => {
+        if (image.dataset.status == "active") {
+          image.dataset.status = "inactive"
+          image.classList.add("hidden")
+          this.imageNumber = parseInt(image.dataset.image, 10) - 1
+        }
+      })
+  
+      this.schoolImageTargets.forEach((image) => {
+        if (image.dataset.image == this.imageNumber) {
+          image.classList.remove("hidden")
+          image.dataset.status = "active"
+        }
+      })
+    }
+
+    if (this.imageNumber == 1) {
+      this.leftArrowSchoolTarget.style.opacity = 0
+    }
+
+    if (this.imageNumber == 3) {
+      this.rightArrowSchoolTarget.style.opacity = 1
     }
   }
 }
